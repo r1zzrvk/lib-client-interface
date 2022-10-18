ec1fcc27f3321d0dac2df29415ee3ed8705894b4
Stanislav <r1zzrvk@gmail.com>
2022-10-18 19:32:49 +0300

    hide Footer for <600px displays

--- src/components/UI/Footer/styled.ts
+++ src/components/UI/Footer/styled.ts
@@ -2,13 +2,17 @@ import { theme } from "@constants"
 import styled from "styled-components"
 
 const Wrapper = styled.div`
+  display: none;
+
+  @media (min-width: ${theme.breakpoints.tablet}px) {
   display: flex;
   justify-content: space-evenly;
+  gap: 120px;
   align-items: stretch;
   text-align: left;
-  gap: 120px;
   background-color: ${theme.colors.main};
   padding: ${theme.space.xl}px;
+  }
 `
 
 export const Styled = {
