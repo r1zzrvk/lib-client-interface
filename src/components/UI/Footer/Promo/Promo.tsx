cbb8f1db52c3e819b289e77783d6aae823100538
Stanislav <r1zzrvk@gmail.com>
2022-10-27 20:10:04 +0300

    refactor: refactor Promo component

--- src/components/UI/Footer/Promo/Promo.tsx
+++ src/components/UI/Footer/Promo/Promo.tsx
@@ -1,22 +1,16 @@
 import { FC } from "react";
 import { Text } from "@components";
 import { theme } from "@constants";
+import { ReadmoreButton } from "./ReadmoreButton";
 
 export const Promo: FC = () => (
   <section>
     <Text
-      marginBottom={theme.space.xl}
-      fontSize={theme.fonts.size.regular.md}
-      fontHeight={theme.fonts.height.regular.md}
-      fontWeight={theme.fonts.weight.medium}>Become a member</Text>
+      marginBottom={theme.space.xl}>Become a member</Text>
     <Text
       marginBottom={theme.space.xl}
       fontSize={theme.fonts.size.regular.sm}
-      fontHeight={theme.fonts.height.regular.sm}>Join our loyalty program now and get 10% off your<br/> next online purchase!</Text>
-       <Text
-      marginBottom={theme.space.sm}
-      fontSize={theme.fonts.size.regular.md}
-      fontHeight={theme.fonts.height.regular.md}
-      fontWeight={theme.fonts.weight.medium}>Read more →</Text>
+      fontHeight={theme.fonts.height.regular.sm}>Join our loyalty program now and get 10% off your<br /> next online purchase!</Text>
+    <ReadmoreButton />
   </section>
 )
---
+++ src/components/UI/Footer/Promo/ReadmoreButton/index.ts
@@ -0,0 +1,1 @@
+export { ReadmoreButton } from './ReadmoreButton'
