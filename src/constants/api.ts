c360b36504d249daabac1009f05b128d96647060
Stanislav <r1zzrvk@gmail.com>
2023-01-28 20:12:24 +0300

    feat: add auth with google, checking production with API

---
+++ src/constants/api.ts
@@ -0,0 +1,1 @@
+export const CLIENT_ID = '1008380682623-7icg3j1r6a509qakdjojp31iebm038q4.apps.googleusercontent.com'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,38 @@
 import { FC } from 'react'
-import { CategoriesBanner, CategoriesSlider, MainBanner, MobileBanner, Input, TipsBanner } from '@components'
+import { CategoriesBanner, CategoriesSlider, MainBanner, MobileBanner, Input, TipsBanner, Button } from '@components'
 import { MobileTemplate } from '@templates'
 import { CATEGORIES_BANNER } from '@constants'
+import { useGoogleLogin } from '@react-oauth/google'
+import axios from 'axios'
 
-const MainPage: FC = () => (
-  <>
-    <MainBanner />
-    <TipsBanner />
-    <CategoriesBanner header={CATEGORIES_BANNER.header} subheader={CATEGORIES_BANNER.subheader} text={CATEGORIES_BANNER.text}/>
-    <MobileTemplate>
-      <Input placeholder="Search" type="text" fluid/>
-      <MobileBanner content='Discounts up to 15% to all'/>
-      <CategoriesSlider />
-    </MobileTemplate>
-  </>
-)
+const MainPage: FC = () => {
+  const login = useGoogleLogin({
+    onSuccess: async codeResponse => {
+      const data = await axios.get('https://www.googleapis.com/oauth2/v3/userinfo', {
+        headers: {
+          "Authorization": `Bearer ${codeResponse.access_token}`
+        }
+      })
+    return console.log(data)
+    }
+  })
 
+  return (
+    <>
+      <MainBanner />
+      <Button onClick={() => login()}>Sign in with Google 🚀 </Button>
+      <TipsBanner />
+      <CategoriesBanner
+        header={CATEGORIES_BANNER.header}
+        subheader={CATEGORIES_BANNER.subheader}
+        text={CATEGORIES_BANNER.text}
+      />
+      <MobileTemplate>
+        <Input placeholder="Search" type="text" fluid />
+        <MobileBanner content="Discounts up to 15% to all" />
+        <CategoriesSlider />
+      </MobileTemplate>
+    </>
+  )
+}
 export default MainPage
