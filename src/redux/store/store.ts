d555b6f95f92ea2a22f76c9291f14b526e0afd0b
Stanislav Vyalkin <svyalkin@mtsbank.ru>
2023-04-23 13:10:38 +0300

    fix: pipeline fix

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,12 +1,12 @@
-import { authReducer } from '@reducers'
-import { configureStore } from '@reduxjs/toolkit'
+// import { authReducer } from '@reducers'
+// import { configureStore } from '@reduxjs/toolkit'
 
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-  },
-})
+// export const store = configureStore({
+//   reducer: {
+//     auth: authReducer,
+//   },
+// })
 
-export type RootState = ReturnType<typeof store.getState>
+// export type RootState = ReturnType<typeof store.getState>
 
-export type AppDispatch = typeof store.dispatch
+// export type AppDispatch = typeof store.dispatch
