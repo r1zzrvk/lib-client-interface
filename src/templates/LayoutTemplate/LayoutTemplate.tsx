d42096711d088a93eecec9235c55d12dc30ddbdb
Stanislav <r1zzrvk@gmail.com>
2023-05-19 17:57:18 +0300

    fix: return changes cus bug reproduced only on mozzila mobile

--- src/templates/LayoutTemplate/LayoutTemplate.tsx
+++ src/templates/LayoutTemplate/LayoutTemplate.tsx
@@ -2,14 +2,13 @@ import { ReactNode, FC } from 'react'
 import { Footer, Header, MobileMenu, Spacer } from '@ui-kit'
 import { PaddingContainer } from '@components/atoms'
 import { theme } from '@constants'
-import { Styled } from './styled'
 
 type TLayoutTemplateProps = {
   children: ReactNode
 }
 
 export const LayoutTemplate: FC<TLayoutTemplateProps> = ({ children }) => (
-  <Styled.Wrapper>
+  <>
     <Header />
     <PaddingContainer padding={theme.space.sm} mobOnly>
       {children}
@@ -17,5 +16,5 @@ export const LayoutTemplate: FC<TLayoutTemplateProps> = ({ children }) => (
     <Spacer sizeMob={theme.space.xl4} size={0} />
     <MobileMenu />
     <Footer />
-  </Styled.Wrapper>
+  </>
 )
