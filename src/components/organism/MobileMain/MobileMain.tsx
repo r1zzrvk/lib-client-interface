f383eeae38e85a74392f0230bc90131fc6347850
Stanislav <r1zzrvk@gmail.com>
2023-05-19 15:55:41 +0300

    refactor(Firefox browser): fix indents and scrollbar in firefox

--- src/components/organism/MobileMain/MobileMain.tsx
+++ src/components/organism/MobileMain/MobileMain.tsx
@@ -1,28 +1,26 @@
 import { theme } from '@constants'
 import { PaddingTemplate } from '@templates'
-import { CategoriesSlider, Input, MobileBanner, MobileCard, PaddingContainer, Spacer } from '@components'
+import { CategoriesSlider, Flexbox, Input, MobileBanner, MobileCard, Spacer } from '@components'
 import { FC, Fragment } from 'react'
 
 export const MobileMain: FC = () => {
   const arr = [1, 2, 3]
 
   return (
-    <PaddingTemplate withoutPadding>
-      <PaddingContainer padding={theme.space.sm}>
-        <Input placeholder="Search" type="text" fluid onChange={() => ''} />
-        <MobileBanner content="Discounts up to 15% to all" />
-      </PaddingContainer>
+    <PaddingTemplate>
+      <Input placeholder="Search" type="text" fluid onChange={() => ''} />
+      <MobileBanner content="Discounts up to 15% to all" />
+      <Spacer size={theme.space.sm} samespace />
       <CategoriesSlider />
-      <PaddingContainer padding={theme.space.sm}>
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
-          {arr.map(item => (
-            <Fragment key={item}>
-              <MobileCard size="lg" imgUrl="https://i.ibb.co/BLTLB6M/photo.png" title="Sofa" />
-              <Spacer sizeMob={16} />
-            </Fragment>
-          ))}
-        </div>
-      </PaddingContainer>
+      <Spacer size={theme.space.sm} samespace />
+      <Flexbox direction="column">
+        {arr.map(item => (
+          <Fragment key={item}>
+            <MobileCard size="lg" imgUrl="https://i.ibb.co/BLTLB6M/photo.png" title="Sofa" />
+            <Spacer sizeMob={theme.space.sm} />
+          </Fragment>
+        ))}
+      </Flexbox>
     </PaddingTemplate>
   )
 }
