79dd92dd01614502046f9a4840b4783d22066de8
Vladimir Maksimov <vmaksimov@almasc.ru>
2023-09-17 16:11:38 +0400

    fix: delete unnecessary for fix build

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,23 +1,22 @@
 import { FC, useState } from 'react'
-import { useRouter } from 'next/router'
 import { ProfileTab, ProfileMenu } from '@components/molecules'
 import { ProfileWrapper } from '@components/atoms'
 import { PROFILE_MENU } from '@constants'
 import { useAppSelector, useBreakpoint } from '@hooks'
-import { EPagePaths, TTab } from '@types'
+import { TTab } from '@types'
 import { getUserData } from '@selectors'
 
 const ProfilePage: FC = () => {
-  const router = useRouter()
+  // const router = useRouter()
   const user = useAppSelector(getUserData)
-  const isAuth = !!user
+  // const isAuth = !!user
   const [selectedTabId, setSelectedTabId] = useState('1')
   const currentTab: TTab | undefined = PROFILE_MENU.find(({ id }) => id === selectedTabId)
   const { isTablet, isSm, isMob } = useBreakpoint()
 
-  if (!isAuth) {
-    router.push(EPagePaths.LOGIN)
-  }
+  // if (!isAuth) {
+  //   router.push(EPagePaths.LOGIN)
+  // }
 
   return (
   <ProfileWrapper>
