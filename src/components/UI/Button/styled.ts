bea6b42d982e6a8d5319040a3b047b7badd7b789
Stanislav Vialkin <r1zzrvk@gmail.com>
2024-05-05 13:21:20 +0300

    fix: add font-weight to buttons

--- src/components/UI/Button/styled.ts
+++ src/components/UI/Button/styled.ts
@@ -14,6 +14,7 @@ const Button = styled.button<TButtonProps>`
   width: ${({ size, isFluid }) => (!isFluid && size ? `${SIZES[size]}px` : '100%')};
   font-size: ${theme.fonts.size.regular.md}px;
   line-height: ${theme.fonts.height.regular.md}px;
+  font-weight: ${theme.fonts.weight.medium};
   font-family: '${theme.fonts.family}', sans-serif;
   color: ${({ isGhost }) => (isGhost ? theme.colors.main : theme.colors.grey)};
   background-color: ${({ isGhost }) => (isGhost ? 'inherit' : theme.colors.secondary)};
--- src/components/molecules/Promo/ReadmoreButton/styled.ts
+++ src/components/molecules/Promo/ReadmoreButton/styled.ts
@@ -9,6 +9,7 @@ const Button = styled.button`
   color: ${theme.colors.white};
   font-size: ${theme.fonts.size.regular.md}px;
   line-height: ${theme.fonts.height.regular.md}px;
+  font-weight: ${theme.fonts.weight.medium};
   font-family: '${theme.fonts.family}', sans-serif;
   transition: 0.4s;
   align-items: center;
