12a10795976f0047f4dede8e903e902e7b34e175
Stanislav Vialkin <r1zzrvk@gmail.com>
2024-08-25 15:49:37 +0300

    fix: add fixed size for Avatar

--- src/components/UI/Avatar/styled.ts
+++ src/components/UI/Avatar/styled.ts
@@ -4,11 +4,17 @@ import { theme } from '@constants'
 
 import { ResponsiveImage } from '../ResponsiveImage'
 
+type TWrapperProps = {
+  size: number
+}
+
 const Avatar = styled(ResponsiveImage)`
   border-radius: ${theme.radiuses.round}px;
 `
 
-const Wrapper = styled.div`
+const Wrapper = styled.div<TWrapperProps>`
+  width: ${({ size }) => size}px;
+  height: ${({ size }) => size}px;
   flex-shrink: 0;
 `
 
